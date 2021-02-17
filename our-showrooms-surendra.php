c0dc894c748d54a0e9ec4068fcf0603acda3b98d
techrupt-devteam <developer@techrupt.in>
2021-02-17 16:26:16 +0530

    initialize repo

---
+++ our-showrooms-surendra.php
@@ -0,0 +1,52 @@
+<?php    
+$title="SEVA Maruti Suzuki Commercial Car Showroom in Nashik, Nagpur, Nanded, Dhule";	
+?>
+
+<?php 
+ $pgDesc="Visit SEVA Automotive's Maruti Suzuki Commercial Car Showroom in Nashik, Nagpur, Dhule, Nanded for the best Maruti Suzuki Mini Trucks, Super carry, Eeco Cargo & Tour cars";
+ 
+ $pgKeywords="Maruti Suzuki, pickup vehicle, commercial truck, small pickup trucks, mini trucks, maruti commercial vehicle, Maruti Suzuki Commercial, commercial passenger vehicle, Maruti Suzuki storage vehicles, Maruti Passenger vehicle, Maruti Suzuki Super Carry, maruti Suzuki mini trucks, mini truck, Super carry, eeco cargo, maruti suzuki commercial in Nashik, maruti suzuki commercial in Nanded, maruti suzuki commercial in Dhule, maruti suzuki commercial in Nagpur";
+ include 'header.php'; 
+?>
+
+<main>
+
+  <div class="container">
+  <div class="my-5"><h2 class="innerpageHeading">Our Showrooms </h2></div>
+	<div class="row">
+    <div class="col-sm-12 col-md-6 col-lg-6">
+    <img src="assets/images/nashik.jpg" class="resposive-img">
+	<div class="my-3">
+    <h4>Nashik</h4>
+    <p>address</p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d119966.09885731817!2d73.69125195840195!3d20.011008034827523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddeb9064d77a7f%3A0xddf402cd2ed0295c!2sNEXA%20Gangapur%20Road%20(Seva%20Automotive%20Pvt.%20Ltd.)!5e0!3m2!1sen!2sin!4v1588142692101!5m2!1sen!2sin" width="100%" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-6">
+    <img src="assets/images/nagpur.jpg" class="resposive-img">
+	<div class="my-3">
+    <h4>Nagpur</h4>
+    <p>address</p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59534.82992098112!2d79.0826189!3d21.1553084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c0e8ae1982c1%3A0x2799eebb2f71f3fb!2sNexa%20Kingsway%20(%20Seva%20Automotive%20Pvt%20Ltd%20)!5e0!3m2!1sen!2sin!4v1588142916020!5m2!1sen!2sin" width="100%" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+    
+  </div>
+  
+  
+  
+</main>
+
+
+<?php  
+require_once('footer.php');
+?>
